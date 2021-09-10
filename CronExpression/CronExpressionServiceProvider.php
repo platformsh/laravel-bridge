15bcea09cb93d066588557d30ed86978f3d01a02
George Birnbaum <gb@glamorise.com>
2021-09-10 08:02:50 -0400

    add cron expression package to extend and define container bindings

--- CronExpression/CronExpressionServiceProvider.php
+++ CronExpression/CronExpressionServiceProvider.php
@@ -2,26 +2,12 @@
 
 namespace Platformsh\LaravelBridge\CronExpression;
 
+use Cron\CronExpression;
 use Illuminate\Support\ServiceProvider;
 
 class CronExpressionServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+    public $bindings = [
+        CronExpression::class => PSHCronExpression::class,
+    ];
 }
