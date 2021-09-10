7daae10d334d18e6da73077a72013d962d44ce6c
George Birnbaum <gb@glamorise.com>
2021-09-10 07:52:04 -0400

    add illuminate support and add service provider

---
+++ CronExpression/CronExpressionServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Platformsh\LaravelBridge\CronExpression;
+
+use Illuminate\Support\ServiceProvider;
+
+class CronExpressionServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
